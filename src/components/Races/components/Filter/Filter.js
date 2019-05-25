ad3d4dfa01f2f3040fb074535d3c77e43171f231
Evgeniy Dainovich <zenbysel@gmail.com>
2019-05-26 00:16:46 +0300

    add basic filter datePicker

--- src/components/Races/components/Filter/Filter.js
+++ src/components/Races/components/Filter/Filter.js
@@ -1,14 +1,37 @@
 import React, {Component} from 'react';
+import DatePicker from 'react-datepicker';
 
+import 'react-datepicker/dist/react-datepicker.css';
 import './style.css';
 
 class Filter extends Component {
+
+  changeDate = () => {
+
+  }
 
   render() {
 
     return (
       <div className={'filter'}>
-        filter
+        <div className={'filter-item'}>
+          <span>Date from</span>
+          <DatePicker
+            className={'date-input'}
+            selected={Date.now()}
+            onChange={this.changeDate}
+          />
+        </div>
+        <div className={'filter-item'}>
+          <span>Date to</span>
+          <DatePicker
+            className={'date-input'}
+            popperClassName={'right-popper-picker'}
+            popperPlacement={'bottom-end'}
+            selected={Date.now()}
+            onChange={this.changeDate}
+          />
+        </div>
       </div>
     );
   }
